445e08d06d6a8b586861391c619bb8d0576978d3
minhgiang9x <minhgiang591@gmail.com>
2025-02-28 17:38:09 +0700

    "commt"

--- src/vonglap_for/vd3.java
+++ src/vonglap_for/vd3.java
@@ -1,6 +1,12 @@
 package vonglap_for;
 import java.util.*;
 public class vd3 {
-    
+    public static void main(String[] args) {
+        int i;// todo Khai báo biến ở phạm vi ngoài vòng lặp
+        for ( i = 1; i <= 5; i++) {
+            System.out.println("Giá trị hiện tại: " + i);
+        }
+        System.out.println("Giá trị hiện tại cuối cùng: " + i); //  → 6
+    }
 
 }
