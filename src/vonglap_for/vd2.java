336065e66dab93f5d554b5e50156890d6c63bc84
minhgiang9x <minhgiang591@gmail.com>
2025-02-28 14:10:40 +0700

    test

---
+++ src/vonglap_for/vd2.java
@@ -0,0 +1,17 @@
+package vonglap_for;
+import java.util.*;
+public class vd2 {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("vui lòng nhập n");
+        int n =scanner.nextInt();
+        int pheptinh =0;
+
+        for(int i=0;i<=10;i++){
+            pheptinh =n*i;
+        System.out.println(n+"*"+i+ "="+pheptinh);}
+
+
+        }
+
+}
