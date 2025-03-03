ce5d114a4ce84527328af50af6fec4a5d462f662
minhgiang9x <minhgiang591@gmail.com>
2025-03-03 15:42:31 +0700

    a

---
+++ src/vonglap_for/vd7.java
@@ -0,0 +1,17 @@
+package vonglap_for;
+import java.util.*;
+public class vd7 {
+    public static void main(String[] args){
+        Scanner scanner=new.Scanner(System.in);
+        System.out.print("vui lòng nhập vào n");
+        int n=scanner.nextInt();
+        scanner.close();
+        if (n<0) {
+            System.out.println("số không hợp lệ"); return;
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=2n-1;j++)
+            }            
+        }
+    }
+
+}
